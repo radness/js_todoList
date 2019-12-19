5b52667822ceae9ce9d75165194704d6974998fa
radness <yhs5128@gmail.com>
2019-12-19 23:39:42 +0900

    commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var btnNew = document.getElementById('btnAdd');
+
+//btnNew.onclick = addNewItem;
+btnNew.onclick = function() {
+    var inputText = document.getElementById('intputText');
+    var itemText = inputText.value;
+    
+    if(!itemText || itemText === "" || itemText === " ")
+        return false;   // blank 방지
+    
+    addNewItem(document.getElementById('todoList'), itemText);
+};
+
+function addNewItem(list, itemText) {
+    var listItem = document.createElement('li');
+    listItem.innerText = itemText;
+    list.appendChild(listItem);
+};
