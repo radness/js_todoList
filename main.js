94bb9d00d3db25f7c1ebf0ddf472f13f6adf7732
radness <yhs5128@gmail.com>
2019-12-23 23:05:34 +0900

    add remove, edit function

--- main.js
+++ main.js
@@ -1,18 +1,75 @@
-var btnNew = document.getElementById('btnAdd');
+var totalItems = 0;
 
-//btnNew.onclick = addNewItem;
-btnNew.onclick = function() {
-    var inputText = document.getElementById('inputText');
-    var itemText = inputText.value;
-    
-    if(!itemText || itemText === "" || itemText === " ")
-        return false;   // blank 방지
-    
-    addNewItem(document.getElementById('todoList'), itemText);
-};
+function updateItemStatus() {
+    var chId = this.id.replace('cb_', '');
+    var itemText = document.getElementById('item_', chId);
 
+    if(this.checked) {
+        itemText.className = 'checkeed';
+    } else {
+        itemText.className = '';
+    }
+}
+
+// edit item
+function editItem() {
+    var newText = prompt("whar should this item be edited to?");
+    if(!newText || newText === "" || newText === " ")
+        return false;
+
+    this.innerText = newText;
+}
+
+// remove item
+function removeItem() {
+    var listItemId = this.id.replace('li_', '');
+    document.getElementById('li_' + listItemId).style.display = "none";
+}
+
+
+// add item
 function addNewItem(list, itemText) {
+    var date = new Date();
+    var id = "" + date.getHours() + date.getMinutes() + date.getSeconds() + date.getMilliseconds();
+
     var listItem = document.createElement('li');
-    listItem.innerText = itemText;
+    listItem.id = 'li_' + id;
+    listItem.ondblclick = removeItem;
+
+    var checkBox = document.createElement('input');
+    checkBox.type = 'checkBox';
+    checkBox.id = 'cb_' + id;
+    checkBox.onclick = updateItemStatus;
+
+    var span = document.createElement('span');
+    span.id = 'item_' + totalItems;
+    span.innerText = itemText;
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(span);
     list.appendChild(listItem);
 };
+
+var inputText = document.getElementById('inputText');
+inputText.focus();
+
+// 엔터키 입력
+inputText.onkeyup = function(event) {
+    // Event.which(13) === Enter Key
+    if(event.which === 13) {
+        var itemText = inputText.value;
+        if(!itemText || itemText === "" || itemText === " ")
+            return false; // blank 방지
+        addNewItem(document.getElementById('todoList'), itemText);
+        inputText.focus();
+        inputText.select();
+    }
+};
+
+var checkBox = document.createElement('input');
+checkBox.type = 'checkbox';
+checkBox.onclick = updateItemStatus;
+
+
+
+
